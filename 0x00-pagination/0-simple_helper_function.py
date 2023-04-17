2444419c8b880a746dadfe84ffc2ed4bae41a08a
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-17 10:24:03 +0300

    simple paging

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,4 +6,4 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple:
     """ Return a tuple """
-    return ((page * page_size) -  page_size, page_size * page)
+    return ((page * page_size) - page_size, page_size * page)
