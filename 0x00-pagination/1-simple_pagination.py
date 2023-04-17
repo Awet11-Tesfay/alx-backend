78a0e787ede2dc1dcafbbd4daf0c9caf98dfd96b
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-17 11:24:48 +0300

    simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -39,6 +39,6 @@ class Server:
         if self.__dataset is None:
             return []
 
-        index_range = index_range(page, page_size)
-        start = self.__dataset[index_range[0]:index_range[1]]
+        index = index_range(page, page_size)
+        start = self.__dataset[index[0]:index[1]]
         return start
