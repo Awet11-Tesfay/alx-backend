b7111e31c739cb0a27dbdbaeeff66c4f4c9cbda2
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-18 18:30:02 +0300

    'basic_dictionary'

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
 """ Caching
 """
-
 from base import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ Inherits from BaseCaching class
-    """
+    """ BaseicCache that inherits from BaseCaching """
 
     def put(self, key, item):
-        """ Assign to dictionary value of the key
-          - if key or item is None
-          - the method should not be do anything
-        """
-        if key and item is not None:
+        """ Assign to the dictionary """
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Must return the value in self.cache_data
-        """
+        """ Return the value in self.cache_data linked to key """
         return self.cache_data.get(key)
