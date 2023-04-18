4e5e3da8b699c7306f916c928004ce8fea667f7d
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-18 22:30:05 +0300

    'fifo'

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,30 +1,24 @@
 #!/usr/bin/env python3
-""" FIFOCache that inherits form BaseCaching
+""" Fifo caching in python
 """
 from base_caching import BaseCaching
 
 
-class FIFICache(BaseCaching):
-    """ Use self.cache_data dictionary from the parent class
-    """
+class BasicCache(BaseCaching):
+    """ BaseicCache that inherits from BaseCaching """
 
-    def __init__(slef):
-        """ Initializing overload but do not forget call parent class
-        """
+    def __init__(self):
         super().__init__()
 
     def put(self, key, item):
-        """ Must assign to self.cache_data the item for key
-        """
+        """ Assign to the dictionary """
         if key is not None and item is not None:
             self.cache_data[key] = item
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            element = list(self.cache_data.keys())
-            del self.cache_data[element[0]]
-            print(f'DISCARD: {element[0]}')
+        if len(self.cache_data) > self.MAX_ITEMS:
+            keys_list = list(self.cache_data.keys())
+            del self.cache_data[keys_list[0]]
+            print(f'DISCARD: {keys_list[0]}')
 
     def get(self, key):
-        """ Return self.cache_data
-        """
+        """ return  the value in self.cache_data """
         return self.cache_data.get(key)
