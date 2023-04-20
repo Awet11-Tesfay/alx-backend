27dedec8d9b755416a24dfdc223278f5680569ee
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-20 03:55:52 +0300

    'mru'

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" Mru Caching in python
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ Inherits from the base class
+    """
+
+    def __init__(self):
+        """ Initialize the instance
+        """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """ Must assign to dicionary
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.keys:
+                self.keys.append(key)
+            else:
+                self.keys.append(self.keys.pop(self.keys.index(key)))
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                discard = self.keys.pop(-2)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
+
+    def get(self, key):
+        """ Must return the value in self.cache_data
+        """
+        if key is not None and key in self.cache_data:
+            self.keys.append(self.keys.pop(self.keys.index(key)))
+            return self.cache_data[key]
+        return None
+   
