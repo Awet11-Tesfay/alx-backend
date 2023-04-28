486e8abcf73b1f38b7905a98c7cf5703442dd5d1
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-28 10:07:08 +0300

    basic_babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Basic Babel setup
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """ Configure the available languages and time
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/')
+def index() -> str:
+    """ The returned html file
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
