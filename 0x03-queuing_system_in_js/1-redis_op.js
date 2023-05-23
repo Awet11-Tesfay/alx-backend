68d7de3c947a2a49384c9786382df51c399bbcb5
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-23 04:57:48 +0300

    basic_node_redis

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,27 @@
+import redis from "redis";
+
+const cli = redis.createClient();
+
+cli.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+cli.on('error', (error) => {
+    console.log(`Redis client not connected to the server : ${error.message}`);
+});
+
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, redis.print)
+}
+
+
+function displaySchoolValue(schoolName) {
+    client.get(schoolName, (err, value) => {
+        console.log(value)
+    });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanfrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
