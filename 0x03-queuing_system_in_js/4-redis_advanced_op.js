9a83c75edbf0246382cb5596f87cef359458fc2e
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-23 05:26:42 +0300

    node_redis_advanced

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -10,4 +10,14 @@ cli.on('error', (error) => {
     console.log(`Redis client not connected to the server : ${error.message}`);
 });
 
-cli.
+cli.hset('Holberton', 'Portland', 50, redis.print);
+cli.hset('Holberton', 'Seattle', 80, redis.print);
+cli.hset('Holberton', 'New York', 20, redis.print);
+cli.hset('Holberton', 'Bogota', 20, redis.print);
+cli.hset('Holberton', 'Cail', 40, redis.print);
+cli.hset('Holberton', 'Paris', 20, redis.print);
+
+cli.hgetall('HolbertonSchoos', (error, objects) => {
+    if (error) console.error(error);
+    else console.log(object);
+})
